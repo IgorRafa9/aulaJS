1a944ae2a3559b1aaaebe46028a1fc3cc67a174e
Igor Rafael <igor_rafa_lima@hotmail.com>
2022-01-22 17:44:28 -0300

    arquivos modificados

--- Js/main.js
+++ Js/main.js
@@ -1,9 +1,30 @@
+function botao(){
+    //alert("Obrigado por clicar")
+    document.getElementById('agradecimento').innerHTML = "<b>Obrigado por clicar</b>";
+    console.log(document.getElementById('agradecimento').value);
+}
+
+function redirecionar(){
+    //window.open("https://www.youtube.com/watch?v=aPTHCcxCIL0")
+    window.location.href = "https://www.youtube.com/watch?v=aPTHCcxCIL0"
+}
+
+function trocar(elemento){
+    //document.getElementById("mousemove").innerHTML = "Obrigado por passar o mouse"
+    elemento.innerHTML = "Obrigado por passar o mouse"
+    //alert("Trocar Texto")
+}
+
+function voltar(elemento){
+    document.getElementById("mousemove").innerHTML = "Passe o mouse aqui"
+    elemento.innerHTML = "Passe o mouse aqui"
+}
 /*function soma(n1, n2){
     return n1+n2;
 }
 alert(soma(5,10));*/
 
-function validarIdade(idade){
+/*function validarIdade(idade){
     var validar;
     if(idade >= 18){
         return true;
@@ -12,7 +33,7 @@ function validarIdade(idade){
 
 }
 var idade = prompt("Qual sua idade?");
-alert(validarIdade(idade));
+alert(validarIdade(idade));*/
 
 /*var d = new Date();
 alert(d.getHours()+":"+d.getMinutes());*/
