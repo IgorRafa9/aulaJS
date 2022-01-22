9ad2c9a0272d1920ca5e9e35483059f9bd462e42
Igor Rafael <igor_rafa_lima@hotmail.com>
2022-01-22 16:27:24 -0300

    criado

---
+++ Js/main.js
@@ -0,0 +1,35 @@
+function soma(n1, n2){
+    
+}
+
+
+
+/*var d = new Date();
+alert(d.getHours()+":"+d.getMinutes());*/
+
+/*var count;
+for(count = 0; count < 5; count++){
+    alert(count);
+}*/
+
+/*var count = 0;
+while(count < 5 ){
+    console.log(count);
+    count++;
+}*/
+
+/*var idade = prompt("Qual sua idade: ");
+if(idade >= 18){
+    alert("Maior de idade")
+}else{
+    alert("Menor de idade")
+};*/
+
+//var lista = ["maça", "pera", "uva"]
+//console.log(lista)
+//var nome = "Igor Rafael";
+//var n1 = 5;
+//var n2 = 3;
+//alert(nome + " tem "+idade+" anos")
+//Number(alert(n1*n2))
+//console.log(nome.replace("Igor Rafael", "Igor"));
